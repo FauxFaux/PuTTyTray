16f019fee7b0ad842b266943024d23e8612c5ad0
Chris West (Faux) <git@goeswhere.com>
2018-04-30 13:23:08 +0100

    url: merge a load of code from t67

--- terminal.c
+++ terminal.c
@@ -12,6 +12,10 @@
 #include "putty.h"
 #include "terminal.h"
 
+
+#include "urlhack.h"
+
+
 #define poslt(p1, p2) ((p1).y < (p2).y || ((p1).y == (p2).y && (p1).x < (p2).x))
 #define posle(p1, p2)                                                          \
   ((p1).y < (p2).y || ((p1).y == (p2).y && (p1).x <= (p2).x))
@@ -4959,6 +4963,52 @@ static void do_paint(Terminal *term, Context ctx, int may_optimise)
 #endif /* OPTIMISE_SCROLL */
   termchar *newline;
 
+
+  int urlhack_underline_always =
+      (conf_get_int(term->conf, CONF_url_underline) ==
+       URLHACK_UNDERLINE_ALWAYS);
+
+  int urlhack_underline =
+      conf_get_int(term->conf, CONF_url_underline) ==
+      URLHACK_UNDERLINE_ALWAYS ||
+      (conf_get_int(term->conf, CONF_url_underline) ==
+       URLHACK_UNDERLINE_HOVER &&
+       (!conf_get_int(term->conf, CONF_url_ctrl_click) ||
+        urlhack_is_ctrl_pressed()))
+      ? 1
+      : 0;
+  int urlhack_is_link = 0, urlhack_hover_current = 0;
+  int urlhack_toggle_x = term->cols, urlhack_toggle_y = term->rows;
+  unsigned int urlhack_region_index = 0;
+  text_region urlhack_region;
+
+  if (term->url_update) {
+    urlhack_reset();
+
+    for (i = 0; i < term->rows; i++) {
+      termline *lp = lineptr(term->disptop + i);
+
+      for (j = 0; j < term->cols; j++) {
+        unsigned long tchar = lp->chars[j].chr;
+        urlhack_putchar(tchar & CHAR_MASK ? (char)(tchar & CHAR_MASK) : ' ');
+      }
+
+      unlineptr(lp);
+    }
+
+    urlhack_go_find_me_some_hyperlinks(term->cols);
+  }
+  urlhack_region = urlhack_get_link_region(urlhack_region_index);
+  urlhack_toggle_x = urlhack_region.x0;
+  urlhack_toggle_y = urlhack_region.y0;
+
+  if (urlhack_underline_always)
+    urlhack_hover_current = 1;
+  else
+    urlhack_hover_current = urlhack_is_in_this_link_region(
+        urlhack_region, urlhack_mouse_old_x, urlhack_mouse_old_y);
+
+
   chlen = 1024;
   ch = snewn(chlen, wchar_t);
 
@@ -5109,6 +5159,45 @@ static void do_paint(Terminal *term, Context ctx, int may_optimise)
       }
       if (j < term->cols - 1 && d[1].chr == UCSWIDE)
         tattr |= ATTR_WIDE;
+
+
+      //  Hyperlink stuff: Underline link regions if user has configured us so
+      if (urlhack_underline) {
+        if (j == urlhack_toggle_x && i == urlhack_toggle_y) {
+          urlhack_is_link = urlhack_is_link == 1 ? 0 : 1;
+
+          // Find next bound for the toggle
+
+          if (urlhack_is_link == 1) {
+            urlhack_toggle_x = urlhack_region.x1;
+            urlhack_toggle_y = urlhack_region.y1;
+
+            if (urlhack_toggle_x == term->cols - 1) {
+              // Handle special case where link ends at the last char of the row
+              urlhack_toggle_y++;
+              urlhack_toggle_x = 0;
+            }
+          } else {
+            urlhack_region = urlhack_get_link_region(++urlhack_region_index);
+
+            if (urlhack_underline_always)
+              urlhack_hover_current = 1;
+            else
+              urlhack_hover_current = urlhack_is_in_this_link_region(
+                  urlhack_region, urlhack_mouse_old_x, urlhack_mouse_old_y);
+
+            urlhack_toggle_x = urlhack_region.x0;
+            urlhack_toggle_y = urlhack_region.y0;
+          }
+        }
+
+        if (urlhack_is_link == 1 && urlhack_hover_current == 1) {
+          tattr |= ATTR_UNDER;
+        }
+
+        term->url_update = 0;
+      }
+
 
       /* Video reversing things */
       if (term->selstate == DRAGGING || term->selstate == SELECTED) {
@@ -5483,7 +5572,12 @@ static void clip_addchar(clip_workbuf *b, wchar_t chr, int attr)
   b->bufpos++;
 }
 
-static void clipme(Terminal *term, pos top, pos bottom, int rect, int desel)
+void clipme(Terminal *term,
+            pos top,
+            pos bottom,
+            int rect,
+            int desel,
+            void (*output)(Terminal *, void *, wchar_t *, int *, int, int))
 {
   clip_workbuf buf;
   int old_top_x;
@@ -5639,7 +5733,7 @@ static void clipme(Terminal *term, pos top, pos bottom, int rect, int desel)
 clip_addchar(&buf, 0, 0);
 #endif
 /* Finally, transfer all that to the clipboard. */
-write_clip(term->frontend, buf.textbuf, buf.attrbuf, buf.bufpos, desel);
+output(term, term->frontend, buf.textbuf, buf.attrbuf, buf.bufpos, desel);
 sfree(buf.textbuf);
 sfree(buf.attrbuf);
 }
@@ -5653,7 +5747,7 @@ void term_copyall(Terminal *term)
   top.x = 0;
   bottom.y = find_last_nonempty_line(term, screen);
   bottom.x = term->cols;
-  clipme(term, top, bottom, 0, TRUE);
+  clipme(term, top, bottom, 0, TRUE, write_clip);
 }
 
 /*
@@ -5941,6 +6035,21 @@ void term_do_paste(Terminal *term)
   queue_toplevel_callback(term_paste_callback, term);
 }
 
+
+void urlhack_launch_url_helper(Terminal *term,
+                               void *frontend,
+                               wchar_t *data,
+                               int *attr,
+                               int len,
+                               int must_deselect)
+{
+  urlhack_launch_url(!conf_get_int(term->conf, CONF_url_defbrowser)
+                     ? conf_get_filename(term->conf, CONF_url_browser)->path
+                     : NULL,
+                     data);
+}
+
+
 void term_mouse(Terminal *term,
                 Mouse_Button braw,
                 Mouse_Button bcooked,
@@ -6109,6 +6218,29 @@ void term_mouse(Terminal *term,
     term->seltype = default_seltype;
     term->selanchor = selpoint;
     term->selmode = SM_CHAR;
+
+
+  } else if (bcooked == MBT_SELECT && a == MA_RELEASE &&
+              term->selstate == ABOUT_TO) {
+    deselect(term);
+    term->selstate = NO_SELECTION;
+
+    if ((!conf_get_int(term->conf, CONF_url_ctrl_click) ||
+         (conf_get_int(term->conf, CONF_url_ctrl_click) &&
+          urlhack_is_ctrl_pressed())) &&
+        urlhack_is_in_link_region(x, y)) {
+      wchar_t *linkbuf = NULL;
+      text_region region = urlhack_get_link_bounds(x, y);
+      pos top, bottom;
+      top.x = region.x0;
+      top.y = region.y0 + term->disptop;
+      bottom.x = region.x1;
+      bottom.y = region.y1 + term->disptop;
+      clipme(term, top, bottom, 0, 0, urlhack_launch_url_helper);
+      sfree(linkbuf);
+    }
+
+
   } else if (bcooked == MBT_SELECT && (a == MA_2CLK || a == MA_3CLK)) {
     deselect(term);
     term->selmode = (a == MA_2CLK ? SM_WORD : SM_LINE);
@@ -6206,7 +6338,8 @@ void term_mouse(Terminal *term,
              term->selstart,
              term->selend,
              (term->seltype == RECTANGULAR),
-             FALSE);
+             FALSE,
+             write_clip);
       term->selstate = SELECTED;
     } else
       term->selstate = NO_SELECTION;
--- windows/window.c
+++ windows/window.c
@@ -228,6 +228,11 @@ static UINT wm_mousewheel = WM_MOUSEWHEEL;
     (wch) <= 0x180D) || /* MONGOLIAN FREE VARIATION SELECTOR */                \
    ((wch) >= 0xFE00 && (wch) <= 0xFE0F)) /* VARIATION SELECTOR 1-16 */
 
+
+
+static int urlhack_cursor_is_hand = 0;
+
+
 const int share_can_be_downstream = TRUE;
 const int share_can_be_upstream = TRUE;
 
@@ -409,6 +414,11 @@ int WINAPI WinMain(HINSTANCE inst, HINSTANCE prev, LPSTR cmdline, int show)
   init_winfuncs();
 
   conf = conf_new();
+
+
+
+  urlhack_init();
+
 
   /*
    * Initialize COM.
@@ -885,6 +895,12 @@ int WINAPI WinMain(HINSTANCE inst, HINSTANCE prev, LPSTR cmdline, int show)
 
   start_backend();
 
+
+
+  urlhack_set_regular_expression(conf_get_int(term->conf, CONF_url_defregex),
+  conf_get_str(term->conf, CONF_url_regex));
+
+
   /*
    * Set up the initial input locale.
    */
@@ -994,6 +1010,11 @@ void cleanup_exit(int code)
   /*
    * Clean up.
    */
+
+
+  urlhack_cleanup();
+
+
   deinit_fonts();
   sfree(logpal);
   if (pal)
@@ -1059,6 +1080,79 @@ static void update_savedsess_menu(void)
   if (sesslist.nsessions <= 1)
     AppendMenu(savedsess_menu, MF_GRAYED, IDM_SAVED_MIN, "(No sessions)");
 }
+
+
+
+void append_url_to_menu(Terminal *term,
+                        void *menu,
+                        wchar_t *data,
+                        int *attr,
+                        int len,
+                        int must_deselect)
+{
+  unsigned int i = 0;
+  BOOL found = FALSE;
+  wchar_t *fixed, *fixing, *search;
+  if (update_url_id > (IDM_URL_MAX - IDM_URL_MIN))
+    return;
+  fixing = fixed = snewn(wcslen(data) + MAX_PATH, wchar_t);
+  search = data;
+
+  if (search[strlen("http:/")] == '/')
+    for (i = 0; i < strlen("http:/"); ++i)
+      *fixing++ = *search++;
+
+  while (*search && i++ < MAX_PATH) {
+    if (!found && *search < 'z' && !url_character_used[*search]) {
+      url_character_used[*search] = TRUE;
+      found = TRUE;
+      *fixing++ = '&';
+    }
+    if (*search == '&')
+      *fixing++ = '&';
+    *fixing++ = *search++;
+  }
+
+  *fixing++ = 0;
+
+  AppendMenuW(url_menu,
+              MF_ENABLED,
+              IDM_URL_MIN + (update_url_id),
+              *fixed ? fixed : data);
+  ++update_url_id;
+  sfree(fixed);
+}
+
+void urlhack_for_every_link(
+    Terminal *term,
+    void (*output)(Terminal *, void *, wchar_t *, int *, int, int));
+
+static void update_url_menu()
+{
+  int i;
+  while (DeleteMenu(url_menu, 0, MF_BYPOSITION))
+    ;
+
+  update_url_id = 0;
+  for (i = 0; i < UCHAR_MAX; ++i)
+    url_character_used[i] = FALSE;
+  url_character_used['/'] = TRUE;
+  url_character_used['.'] = TRUE;
+
+  urlhack_for_every_link(&append_url_to_menu);
+}
+
+void url_menu_find_and_launch(Terminal *term,
+                              void *menu,
+                              wchar_t *data,
+                              int *attr,
+                              int len,
+                              int must_deselect)
+{
+  if (search_url_id++ == url_target_id)
+    urlhack_launch_url_helper(term, menu, data, attr, len, must_deselect);
+}
+
 
 /*
  * Update the Special Commands submenu.
@@ -2244,6 +2338,8 @@ static LRESULT CALLBACK WndProc(HWND hwnd,
       get_sesslist(&sesslist, TRUE);
       update_savedsess_menu();
       return 0;
+    } else if ((HMENU)wParam == url_menu) {
+      update_url_menu();
     }
     break;
   case WM_COMMAND:
@@ -2409,6 +2505,13 @@ static LRESULT CALLBACK WndProc(HWND hwnd,
       /* Pass new config data to the back end */
       if (back)
         back->reconfig(backhandle, conf);
+
+
+      urlhack_set_regular_expression(conf_get_int(conf, CONF_url_defregex),
+          conf_get_str(conf, CONF_url_regex));
+
+      term->url_update = TRUE;
+
 
       /* Screen size changed ? */
       if (conf_get_int(conf, CONF_height) !=
@@ -2584,6 +2687,15 @@ static LRESULT CALLBACK WndProc(HWND hwnd,
         if (back)
           back->special(backhandle, specials[i].code);
       }
+
+
+      if (wParam >= IDM_URL_MIN && wParam < (IDM_URL_MIN + update_url_id)) {
+        url_target_id = wParam - IDM_URL_MIN;
+        search_url_id = 0;
+        urlhack_for_every_link(&url_menu_find_and_launch);
+      }
+
+
     }
     break;
 
@@ -2737,6 +2849,44 @@ static LRESULT CALLBACK WndProc(HWND hwnd,
      * number noise.
      */
     noise_ultralight(lParam);
+
+
+    if (urlhack_mouse_old_x != TO_CHR_X(X_POS(lParam)) ||
+        urlhack_mouse_old_y != TO_CHR_Y(Y_POS(lParam))) {
+      urlhack_mouse_old_x = TO_CHR_X(X_POS(lParam));
+      urlhack_mouse_old_y = TO_CHR_Y(Y_POS(lParam));
+
+      if ((!conf_get_int(term->conf, CONF_url_ctrl_click) ||
+           urlhack_is_ctrl_pressed()) &&
+          urlhack_is_in_link_region(urlhack_mouse_old_x, urlhack_mouse_old_y)) {
+        if (urlhack_cursor_is_hand == 0) {
+          SetClassLongPtr(
+              hwnd,
+              GCLP_HCURSOR,
+              (LONG_PTR)LoadCursor(NULL, MAKEINTRESOURCE(IDC_HAND)));
+          urlhack_cursor_is_hand = 1;
+          term_update(
+              term); // Force the terminal to update, otherwise the underline
+                     // will not show (bug somewhere, this is an ugly fix)
+        }
+      } else if (urlhack_cursor_is_hand == 1) {
+        SetClassLongPtr(hwnd,
+                        GCLP_HCURSOR,
+                        (LONG_PTR)LoadCursor(NULL, MAKEINTRESOURCE(IDC_IBEAM)));
+        urlhack_cursor_is_hand = 0;
+        term_update(term); // Force the terminal to update, see above
+      }
+
+      // If mouse jumps from one link directly into another, we need a forced
+      // terminal update too
+      if (urlhack_is_in_link_region(urlhack_mouse_old_x, urlhack_mouse_old_y) !=
+          urlhack_current_region) {
+        urlhack_current_region =
+            urlhack_is_in_link_region(urlhack_mouse_old_x, urlhack_mouse_old_y);
+        term_update(term);
+      }
+    }
+
 
     if (wParam & (MK_LBUTTON | MK_MBUTTON | MK_RBUTTON) &&
         GetCapture() == hwnd) {
@@ -3222,10 +3372,31 @@ static LRESULT CALLBACK WndProc(HWND hwnd,
       }
     }
     return FALSE;
+
+
   case WM_KEYDOWN:
-  case WM_SYSKEYDOWN:
+    if (wParam == VK_CONTROL && conf_get_int(term->conf, CONF_url_ctrl_click)) {
+      GetCursorPos(&cursor_pt);
+      ScreenToClient(hwnd, &cursor_pt);
+
+      if (urlhack_is_in_link_region(TO_CHR_X(cursor_pt.x),
+                                    TO_CHR_Y(cursor_pt.y))) {
+        SetCursor(LoadCursor(NULL, IDC_HAND));
+        term_update(term);
+      }
+      goto KEY_END;
+    } // fallthrough
   case WM_KEYUP:
+    if (wParam == VK_CONTROL && conf_get_int(term->conf, CONF_url_ctrl_click)) {
+      SetCursor(LoadCursor(NULL, IDC_IBEAM));
+      term_update(term);
+      goto KEY_END;
+    } // fallthrough
+  KEY_END:
+  case WM_SYSKEYDOWN:
   case WM_SYSKEYUP:
+
+
     /*
      * Add the scan code and keypress timing to the random
      * number noise.
@@ -5169,8 +5340,12 @@ void write_aclip(void *frontend, char *data, int len, int must_deselect)
 /*
  * Note: unlike write_aclip() this will not append a nul.
  */
-void write_clip(
-    void *frontend, wchar_t *data, int *attr, int len, int must_deselect)
+void write_clip(Terminal *term,
+                void *frontend,
+                wchar_t *data,
+                int *attr,
+                int len,
+                int must_deselect)
 {
   HGLOBAL clipdata, clipdata2, clipdata3;
   int len2;
